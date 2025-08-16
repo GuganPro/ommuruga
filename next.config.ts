d9eb93e5eae3bff5b3237d9e1b7a901c7889d930
GuganPro <guganvs2@gmail.com>
2025-08-16 13:17:59 +0530

    Add files via upload
    new

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    NEXT_PUBLIC_USE_MONGODB: process.env.USE_MONGODB,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
